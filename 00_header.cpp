f7ffa0ac847c286fda0e6da1a906b749c9f2a98c
Kazip0 <55454484+Kazip0@users.noreply.github.com>
2022-07-08 22:11:07 +0530

    Add files via upload

---
+++ 00_header.cpp
@@ -0,0 +1,17 @@
+/*
+There are 2 types of header files
+1. System Header files (that comes along with the compiler)
+2. User defined Header files (written by the user)
+
+
+website for looking up all the header files
+https://en.cppreference.com/w/cpp/header
+
+deprecated header files means that the libraries are removed or changed to a different name
+
+
+Operators in c++ : check out the other programming folder of beginners
+
+*/
+
+
