8275085cd2fb3bc6d276ca9534e6f33b3c654f07
Kazip0 <55454484+Kazip0@users.noreply.github.com>
2022-07-10 18:26:04 +0530

    Add files via upload

---
+++ 01c_if_else.cpp
@@ -0,0 +1,23 @@
+// if else statement
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    if(num %2 == 0)
+    {
+        cout<<"It's an even number" << endl;
+        
+    }
+    else
+    {
+        cout << "It's an odd number" << endl;
+    }
+    cout << "Thanks";
+}
+
